81d81018ef847767a0b63bef0486a83383d2684f
Harland <lohoraharland@gmail.com>
2018-05-19 11:53:59 -0500

    Product Added Successfully

--- js/App.js
+++ js/App.js
@@ -17,16 +17,30 @@ class UI{
         <strong>Price</strong>: ${product.price}
         <strong>Quantity</strong>: ${product.quantity}
         <strong>Year</strong>: ${product.year}
+        <a href="#" class="btn btn-danger" name="delete">Delete</a>
       </div>
     <div>`;
     productList.appendChild(element);
   }
-  
-
-  deleteProduct(){
-
+  resetForm(){
+    document.getElementById("product-form").reset();
   }
-  showMessage(){
+
+  deleteProduct(element){
+    if(element.name === "delete"){
+      element.parentElement.parentElement.parentElement.remove();
+    }
+  }
+  showMessage(message){
+    const div = document.createElement("div");
+    div.className = "alert alert-success mt-4";
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const app = document.querySelector("#app");
+    container.insertBefore(div, app);
+    setTimeout(function(){
+      document.querySelector('.alert').remove();
+    },3000);
 
   }
 }
@@ -39,6 +53,12 @@ document.getElementById('product-form').addEventListener("submit",function(e){
   const product = new Product( productName,productPrice,productQuan,productYear);
   const ui      = new UI();
   ui.addProduct(product);
+  ui.resetForm();
+  ui.showMessage("Product added sucessfully");
   e.preventDefault();
+});
 
+document.getElementById("product-list").addEventListener("click",function(e){
+  const ui = new UI();
+  ui.deleteProduct(e.target)
 });
