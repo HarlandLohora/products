42eedd89f061a77eb09f337cd6f01aa0d4de1e14
Harland <lohoraharland@gmail.com>
2018-05-19 13:45:47 -0500

    Almost

--- js/App.js
+++ js/App.js
@@ -29,11 +29,12 @@ class UI{
   deleteProduct(element){
     if(element.name === "delete"){
       element.parentElement.parentElement.parentElement.remove();
+      this.showMessage("Product deleted","danger");
     }
   }
-  showMessage(message){
+  showMessage(message,cssClass){
     const div = document.createElement("div");
-    div.className = "alert alert-success mt-4";
+    div.className = "alert alert-"+cssClass+" mt-4";
     div.appendChild(document.createTextNode(message));
     const container = document.querySelector(".container");
     const app = document.querySelector("#app");
@@ -54,7 +55,7 @@ document.getElementById('product-form').addEventListener("submit",function(e){
   const ui      = new UI();
   ui.addProduct(product);
   ui.resetForm();
-  ui.showMessage("Product added sucessfully");
+  ui.showMessage("Product added sucessfully","success");
   e.preventDefault();
 });
 
